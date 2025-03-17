b025a0270f4e0e1cbbe88c1aa319ab9759e2fabf
Kirill Pershin <trasa1111@mail.ru>
2025-03-17 22:41:54 +0300

    HotFix subjectName

--- internal/repository/postgres/homework/get_by_group_and_time.go
+++ internal/repository/postgres/homework/get_by_group_and_time.go
@@ -9,7 +9,7 @@ import (
 )
 
 func (r *Repo) GetByGroupAndTime(ctx context.Context, userID entity.UserID, groupID entity.GroupID, fromTime time.Time, toTime time.Time) ([]entity.Homework, error) {
-	q := "SELECT h.*," +
+	q := "WITH tmp AS(SELECT h.*," +
 		"COALESCE(EXISTS (" +
 		"SELECT 1 FROM homeworkstatuses AS hs " +
 		"WHERE hs.homework_id = h.homework_id AND hs.user_id = $4" +
@@ -17,8 +17,8 @@ func (r *Repo) GetByGroupAndTime(ctx context.Context, userID entity.UserID, grou
 		"FROM homeworks AS h " +
 		"WHERE h.group_id = $1 " +
 		"AND h.due_date >= $2 " +
-		"AND h.due_date <= $3"
-
+		"AND h.due_date <= $3) " +
+		"SELECT tmp.*, subject_name FROM tmp JOIN subjects ON tmp.subject_id = subjects.subject_id"
 
 	var homeworks []homework
 
